e63a7cea90037a91b7d87981e23cf0cdc18aa72f
Jason Ogasian <jogasian@book.com>
2014-06-02 16:15:28 -0700

    Added defaults and more documentation.

--- README.md
+++ README.md
@@ -1,4 +1,80 @@
-redis-sentinel-client
-=====================
+redis-clustering-client
+=======================
 
-A client for communicating with a Redis Cluster via the cluster's Sentinel applications.
+A client for communicating with a Redis Cluster via the cluster's Sentinel 
+applications.
+
+This module abstracts away the concept of a cluster to allow users to connect to 
+the cluster master regardless of any failovers that have occurred since the last 
+connection.  
+You also have the option of connecting to a random slave of the cluster as well.  
+
+
+Usage:
+------
+
+A simple example:
+
+```js
+  var Sentinel = require('./redis-sentinel.js');
+
+  // Initialize to use all known Sentinels
+  Sentinel.init('mymaster', 
+  [
+    { host:'192.168.1.1', port:26379},
+    { host:'anotherHost', port:26380},
+    { host:'localhost', port:26379}
+  ], ready);
+
+
+  function ready(err) {
+    if (err) {
+      console.log('ERROR: ' + err);
+      return;
+    }
+
+    // Connect to the cluster master and set a value
+    Sentinel.command('set', ['clustering', 'is cool!'], function(err, resp) {
+      if (err) {
+        console.log('ERROR: ' + err);
+        return;
+      }
+    });
+
+    // Connect to a random slave and get a hash value
+    Sentinel.command('hget', ['fuster', 'cluck'], function(err, resp) {
+      if (err) {
+        console.log('ERROR: ' + err);
+        return;
+      }
+      console.log(resp);
+    }, true);
+  }
+```  
+
+
+API:
+----
+
+###cluster.init(cluster, sentinelArray, [callback])
+Provide an ordered array of sentinels to use for connections. A connection to 
+the reported Master Redis server is also established as a check.
+
+* `cluster`: the name of the Redis Cluster in sentinel.conf (default: mymaster)
+* `sentinelArray`: an array of objects describing each Redis Sentinel to attempt 
+a connection with.  
+*Example (default):* 
+```js
+    [{host:"localhost", port:26379}]
+```
+* `callback([err])`: called when test connection to master is completed or an error 
+occurs
+
+###cluster.command(cmd, args, [callback, slave])
+Send a Redis command to the cluster master (or slave if `slave == true`) and 
+receive the response.
+
+* `cmd`: redis command to execute
+* `args`: array of arguments to the command
+* `callback(err, [res])`: response from the redis server
+* `slave`: boolean - if true send command to a random slave
--- redis-sentinel.js
+++ redis-sentinel.js
@@ -1,36 +1,74 @@
+/**
+ * Module pretty-logs
+ *
+ * Creates nicely formatted console logs with a context (breadCrumb) and message
+ * Created By: Jason Ogasian
+ * Created On: 5-31-2014
+ *
+ * ISC License (ISC)
+ *
+ * Copyright (c) 2014, Jason Ogasian
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any 
+ * purpose with or without fee is hereby granted, provided that the above 
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY 
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, 
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM 
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR 
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
 var redis = require('redis');
 
 var Sentinel = {
+
   //
-  // Properties
+  // Default properties
   //
-  cluster: '',
-  sentinels: [],
+  cluster: 'mymaster',
+  sentinels: [{host:"localhost", port:26379}],
   preferredSentinel: null,
 
 
-  //
-  // Functions
-  //
+  /**
+   * Initialize the Sentinel object to use the given Sentinels. Also verifies a 
+   * connection to the cluster master.
+   * @param  {String}   cluster       Name of the Redis cluster (i.e. mymaster)
+   * @param  {Array}    sentinelArray Array of Sentinels [{host:'abc',port:123}]
+   * @param  {Function} callback      callback([err])
+   */
   init: function(cluster, sentinelArray, callback) {
     this.cluster = cluster;
 
     sentinelArray.forEach(function(s) {
       this.sentinels.push(s);
     }, this);
-    this.connect(function(err, masterClient) {
-      if (err) callback('(init) ' + err);
-      else {
-        masterClient.quit();
-        callback();
+    connect(this, function(err, masterClient) {
+      if (err && callback) {
+        callback('(init) ' + err);
+        return;
       }
+
+      masterClient.quit();
+      if (callback) callback();
     });
   },
 
+
+  /**
+   * Send a Redis command to the cluster master (or random slave if desired).
+   * @param  {String}   cmd      Any Redis command supported by redis module
+   * @param  {Array}    args     An array of arguments to the given command
+   * @param  {Function} callback [optional] callback(err, [resp])
+   * @param  {Boolean}  slave    Connect to a random slave instead of master
+   */
   command: function(cmd, args, callback, slave) {
-    this.connect(function(err, redisClient) {
-      if (err) {
-        if (callback) callback('(command) ' + err);
+    connect(this, function(err, redisClient) {
+      if (err && callback) {
+        callback('(command) ' + err);
         return;
       }
 
@@ -39,59 +77,89 @@ var Sentinel = {
         if (callback) callback(err, resp);
       });
     }, slave);
-  },
-
-  connect: function(callback, slave) {
-    var self = this;
-    var attempts = 0;
-
-    if (self.preferredSentinel) {
-      sentinelConnect(self.preferredSentinel, function(err, client) {
-        if (err) trySentinel();
-        else (callback('', client));
-      });
-    }
-    else {
-      trySentinel();
-    }
-
-    function trySentinel() {
-      if (attempts >= self.sentinels.length) {
-        callback('(connect) Unable to connect to a Sentinel');
-        return;
-      }
-      var s = self.sentinels[attempts];
-
-      sentinelConnect(s, function(err, client) {
-        if (err) {
-          console.log('\tConnection Error: ' + err);
-          trySentinel(++attempts);
-        }
-        else if (client) {
-          attempts = self.sentinels.length; // Break the loop
-          callback('', client);
-        }
-      });
-    }
-
-    function sentinelConnect(sen, callback) {
-      var sentinelClient = redis.createClient(sen.port, sen.host, {max_attempts:1});
-      sentinelClient.on('error', function(err) { callback('(sentinelConnect) ' + err); });
-      sentinelClient.on('ready', function() {
-        self.preferredSentinel = sen;
-
-        if (slave) {
-          getSlaveClient(sentinelClient, self.cluster, callback);
-        }
-        else {
-          getMasterClient(sentinelClient, self.cluster, callback);
-        }
-      });
-    }
   }
 }
 
 
+
+
+/**
+ * Connect to a Redis master (or slave) as determined by a sentinel
+ * @param  {Object}   that     Sentinel object that called this function
+ * @param  {Function} callback callback(err, [RedisClient])
+ * @param  {Boolean}  slave    Connect to a random slave instead of master
+ */
+function connect(that, callback, slave) {
+  var attempts = 0; // Count the sentinel connection attempts
+
+  if (that.preferredSentinel) {
+    sentinelConnect(that.preferredSentinel, function(err, client) {
+      if (err) trySentinel();
+      else (callback('', client));
+    });
+  }
+  else {
+    trySentinel();
+  }
+
+
+  /**
+   * Loop through all known Sentinels until a connection is made - or error
+   */
+  function trySentinel() {
+
+    if (attempts >= that.sentinels.length) {
+      callback('(connect) Unable to connect to a Sentinel');
+      return;
+    }
+    var s = that.sentinels[attempts];
+
+    sentinelConnect(s, function(err, client) {
+      if (err) {
+        console.log('\tConnection Error: ' + err);
+        attempts++;
+        trySentinel();
+      }
+      else if (client) {
+        attempts = that.sentinels.length; // Break the loop
+        callback('', client);
+      }
+    });
+  }
+
+
+  /**
+   * Attempt a connection to the given Redis Sentinel
+   * @param  {Object}   sen      Sentinel: {host:'abc', port:123}
+   * @param  {Function} callback callback(err, [RedisClient])
+   */
+  function sentinelConnect(sen, callback) {
+
+    var sentinelClient = redis.createClient(sen.port, sen.host, 
+      {max_attempts:1, connect_timeout:5000});
+    sentinelClient.on('error', function(err) { callback('(sentinelConnect) ' + err); });
+    sentinelClient.on('ready', function() {
+
+      // Use this sentinel first for the next connection attempt
+      that.preferredSentinel = sen;
+
+      if (slave) {
+        getSlaveClient(sentinelClient, that.cluster, callback);
+      }
+      else {
+        getMasterClient(sentinelClient, that.cluster, callback);
+      }
+    });
+  }
+}
+
+
+/**
+ * Create a connection client to the master specified by the given Sentinel
+ * @param  {RedisClient} sentinelClient Client connection to a Sentinel
+ * @param  {String}      cluster        Name of the Redis cluster (i.e. mymaster)
+ * @param  {Function}    callback       callback(err, [RedisClient])
+ */
 function getMasterClient(sentinelClient, cluster, callback) {
   sentinelClient.send_command('SENTINEL', ['get-master-addr-by-name', cluster],
     function(err, master) {
@@ -110,6 +178,12 @@ function getMasterClient(sentinelClient, cluster, callback) {
 }
 
 
+/**
+ * Create a connection client to a random slave specified by the given Sentinel
+ * @param  {RedisClient} sentinelClient Client connection to a Sentinel
+ * @param  {String}      cluster        Name of the Redis cluster (i.e. mymaster)
+ * @param  {Function}    callback       callback(err, [RedisClient])
+ */
 function getSlaveClient(sentinelClient, cluster, callback) {
   sentinelClient.send_command('SENTINEL', ['slaves', cluster],
     function(err, slaves) {
@@ -140,11 +214,14 @@ function getSlaveClient(sentinelClient, cluster, callback) {
               break;
           }
 
+          // Every other line in the array is a value of the previous key
           function storeNext() {
             tmpSlave[config] = slave[index+1];
           }
         });
         
+
+        // Only consider slaves that are online and valid
         if (tmpSlave['role-reported'] == 'slave' && 
             tmpSlave['master-link-status'] == 'ok') {
           goodSlaves.push(tmpSlave);
@@ -162,8 +239,12 @@ function getSlaveClient(sentinelClient, cluster, callback) {
 }
 
 
-// Returns a random integer between min and max
-// Using Math.round() will give you a non-uniform distribution!
+/**
+ * Generates a random integer between min and max
+ * @param  {Number} min Minimum value to output (inclusive)
+ * @param  {Number} max Maximum value to output (inclusive)
+ * @return {Number}     Random value
+ */
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
